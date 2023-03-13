c8e829a429ac1fffb51866454b1f11441fb6dac1
Neeraj <ngupta.gupta53@gmail.com>
2023-03-13 19:06:22 +0530

    palindrom added

--- app/src/main/java/com/ds/programms/string_programm/Palindrome.kt
+++ app/src/main/java/com/ds/programms/string_programm/Palindrome.kt
@@ -1,11 +1,14 @@
 package com.ds.programms.string_programm
 
+import android.annotation.SuppressLint
+
 /**
  * Created by 1000292 on 19-11-2022
  * BFDL
  * PUNE.
  */
 
+@SuppressLint("SuspiciousIndentation")
 fun palindrome(str: String):Boolean{
   var reverse=""
     (str.length-1 downTo 0).forEach { i ->
